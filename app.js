894e6b1c7b055723987a9b3bb953b293ba6fe0e4
stevie2codes <stevie2codes@gmail.com>
2020-11-02 16:27:47 -0600

    add headers

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/',(req,res) => {
-	res.send('Hello from the WebbDev!');
-})
+app.get('/', (req, res) => {
+  res.send('Hello from the WebbDev!');
+});
+app.get('/demo', (req, res) => {
+  res.set('X-WebbDev', '4life');
+  res.status(418);
+  res.send('I prefer coffee');
+});
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`));
