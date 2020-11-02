85bdc0cea6009b86829fd27de77163527056a878
stevie2codes <stevie2codes@gmail.com>
2020-11-02 17:59:01 +0000

    wip

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/',(req,res) => {
-	res.send('Hello from the WebDev!');
+	res.send('Hello from the WebbDev!');
 })
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
