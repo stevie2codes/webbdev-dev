3e4e8da8bf9f3649dad53b5552652b38a2b453ba
stevie2codes <stevie2codes@gmail.com>
2020-11-03 16:42:50 +0000

    express router

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const router = express.Router();
+const path = require('path');
 
-app.get('/', (req, res) => {
-  res.send('Hello from the WebbDev!');
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname+ 'index.html'));
 });
 app.get('/demo', (req, res) => {
   res.set('X-WebbDev', '4life');
@@ -11,4 +13,5 @@ app.get('/demo', (req, res) => {
   res.send('I prefer coffee');
 });
 
+app.use('/',router)
 app.listen(port, () => console.log(`App listening on port ${port}!`));
