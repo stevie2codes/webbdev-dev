d5b5c6fccb9a4ef4d3fc197474f1ba510eb767fb
stevie2codes <stevie2codes@gmail.com>
2020-11-03 20:59:25 +0000

    fix app.js

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/', (req, res) => {
-<<<<<<< HEAD
-  res.sendFile(path.join(__dirname,'ui/html/index.html'));
-=======
+
   res.sendFile(path.join(__dirname, 'Front-End/html/index.html'));
->>>>>>> 63f2d6b2db759b541ca88f6f71328a453c225fc1
+
 });
 app.get('/demo', (req, res) => {
   res.set('X-WebbDev', '4life');
@@ -17,9 +15,9 @@ app.get('/demo', (req, res) => {
   res.send('I prefer coffee');
 });
 
-<<<<<<< HEAD
-app.use('/',router)
-=======
+
+
+
 app.use('/', router);
->>>>>>> 63f2d6b2db759b541ca88f6f71328a453c225fc1
+
 app.listen(port, () => console.log(`App listening on port ${port}!`));
